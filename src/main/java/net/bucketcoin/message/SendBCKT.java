f766792a22c12a243540fe50d32c93f2c1bc364f
Geruls <Gerard.sayson.25@gmail.com>
2021-09-08 21:31:57 +0800

    split into constructor and method, and add charset specification

--- src/main/java/net/bucketcoin/message/SendBCKT.java
+++ src/main/java/net/bucketcoin/message/SendBCKT.java
@@ -11,26 +11,33 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class SendBCKT extends Message {
 
-    public SendBCKT(@NotNull Wallet sender, @NotNull Wallet recipient, int bckt, int gas_fee) throws
+    private final Wallet sender;
+    private final Wallet recipient;
+    private final double bckt;
+    private final double gas_fee;
 
-            NoSuchPaddingException,
-            NoSuchAlgorithmException,
-            InvalidKeyException,
-            IllegalBlockSizeException,
-            BadPaddingException {
+    public SendBCKT(@NotNull Wallet sender, @NotNull Wallet recipient, double gas_fee, double bckt) {
 
-        super(sender, recipient, gas_fee);
+        super(sender, recipient, gas_fee, bckt);
+        this.sender = sender;
+        this.recipient = recipient;
+        this.bckt = bckt;
+        this.gas_fee = gas_fee;
+    }
+
+    @Override
+    public void send() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Transaction transaction = new Transaction(bckt, sender.publicKey.toString(), recipient.publicKey.toString(), gas_fee);
         String transaction_sha256 = DigestUtils.shaHex(transaction.toString());
         var c = Cipher.getInstance("RSA");
         c.init(Cipher.ENCRYPT_MODE, sender.getPrivateKey());
-        var signature = c.doFinal(transaction_sha256.getBytes());
+        var signature = c.doFinal(transaction_sha256.getBytes(StandardCharsets.US_ASCII));
         Broadcast.transaction(transaction, sender.publicKey, signature); // network effect
     }
-
 }

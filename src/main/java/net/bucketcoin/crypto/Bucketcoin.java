da62b956dc4c53cc8da9d8a0715641ee4f16428e
Geruls <Gerard.sayson.25@gmail.com>
2021-09-13 19:54:43 +0800

    Add threads for difficulty calculation and duration counting

--- src/main/java/net/bucketcoin/crypto/Bucketcoin.java
+++ src/main/java/net/bucketcoin/crypto/Bucketcoin.java
@@ -3,8 +3,13 @@ package net.bucketcoin.crypto;
 import net.bucketcoin.block.Block;
 import net.bucketcoin.block.Transaction;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static net.bucketcoin.algorithm.DifficultyAlgorithm.calculateDifficulty;
 
 /**
  * The Bucketcoin blockchain class.
@@ -23,7 +28,10 @@ public final class Bucketcoin {
     /**
         The chain is a singleton.
      */
-    private Bucketcoin() {
+    private Bucketcoin() throws SecurityException {
+
+        AtomicBoolean threadInterrupted = new AtomicBoolean(false);
+        AtomicLong durationUntil1500thBlock = new AtomicLong(0);
 
         // Genesis block.
         chain.push(new Block("", new ArrayList<>(){{
@@ -34,8 +42,43 @@ public final class Bucketcoin {
 
         }}));
 
+        var t = new Thread(null, () -> {
+            int x = chain.size();
+            boolean k = false;
+            while(!threadInterrupted.get()) {
+                if(x % 1500 == 0 && !k && x != 0) {
+                    calculateDifficulty(1500, Duration.ofSeconds(durationUntil1500thBlock.getPlain()));
+                    k = true;
+                    ++x;
+                } else if(x % 1500 == 0 && k && x != 0
+                || k && x == 0) {
+                    x = 0;
+                } else {
+                    x = x + getDiff(x, chain.size() % 1500);
+                }
+            }
 
+        }, "difficulty");
 
+        var t2 = new Thread(null, () -> {
+
+            while(!threadInterrupted.get()) {
+                if(chain.size() % 1500 == 0) durationUntil1500thBlock.setPlain(0);
+                durationUntil1500thBlock.getAndIncrement();
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(1000);
+                } catch(InterruptedException e) {
+                    threadInterrupted.set(true);
+                }
+            }
+
+        }, "duration");
+
+    }
+
+    private int getDiff(int a, int b) {
+        return Math.abs(a - b);
     }
 
     /**
